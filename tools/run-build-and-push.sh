2509bde417de7918b9257799db4b0198c4949cf7
lambdamusic <michele.pasin@gmail.com>
2021-08-24 17:22:50 +0100

    automated docs release

--- tools/run-build-and-push.sh
+++ tools/run-build-and-push.sh
@@ -2,26 +2,11 @@
 # run mkdocs site in local
 # prerequisites: chmod u+x <filename>
 
-echo "=================="
-echo "Building '/mkdocs/src-docs' into '../docs' folder..."
-echo "=================="
-
-cd mkdocs
-mkdocs build --clean
-
-echo "=================="
-echo "Copying CNAME settings to '../docs' folder..."
-echo "=================="
-
-cp CNAME ../docs
-
+./tools/run-build.sh
 
 echo "=================="
 echo "Pushing to Github..."
 echo "=================="
-
-
-cd "$TARGETDIR"
 
 git add -A
 
