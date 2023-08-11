61ce1259684a6c35325c1ab064b4342a91ca69fa
lambdamusic <michele.pasin@gmail.com>
2023-08-11 11:53:11 +0100

    cd index materials

---
+++ archive/CD-index/CDindex_query3_pubmed.sql
@@ -0,0 +1,39 @@
+CREATE OR REPLACE TABLE `reports-dimensions-ai.precomputed.publications_cd_index_pubmed`
+CLUSTER BY id
+AS
+(
+  WITH publications AS
+  (
+    SELECT id, year, citations, reference_ids
+    FROM `dimensions-ai.data_analytics.publications`
+    WHERE year IS NOT NULL AND pmid IS NOT NULL
+  )
+
+  SELECT focal_id AS id,
+  (SUM(score)/COUNT(DISTINCT citation_id))+2 AS cd_5,
+  COUNTIF(score = -1)*((SUM(score)/COUNT(DISTINCT citation_id))+2) AS mcd_5
+  FROM
+  (
+    (
+      SELECT DISTINCT publications.id AS focal_id,
+      citation.id AS citation_id,
+      -1 AS score
+      FROM publications
+      LEFT JOIN UNNEST(publications.citations) AS citation
+      WHERE citation.year - publications.year BETWEEN 1 AND 5
+    )
+    UNION ALL
+    (
+      SELECT DISTINCT publications.id AS focal_id,
+      reference_citation.id as citation_id,
+      -2 as score
+      FROM publications
+      LEFT JOIN UNNEST(publications.reference_ids) AS reference_id
+      INNER JOIN publications AS references
+      ON references.id = reference_id
+      LEFT JOIN UNNEST(references.citations) AS reference_citation
+      WHERE reference_citation.year - publications.year BETWEEN 1 AND 5
+    )
+  )
+  GROUP BY 1
+)
---
+++ archive/CD-index/README.md
@@ -0,0 +1,9 @@
+# CD Index calculations
+
+The notebooks in this folder present a method to compute the CD index via a short SQL query on Dimensions on Google BigQuery. This approach makes it possible to calculate e.g. CD5 index for all journal articles with references in Dimensions in less than 5 hours.
+
+**Note** 
+
+These notebooks are supplementary materials to the paper (soon to be released):
+
+* A fast way to compute the CD index, Joerg Sixt, Michele Pasin, 2023.
